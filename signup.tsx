db489b01ce0e12be761749611be56fa13908a103
Hamani_Younes <68382115+younesazz@users.noreply.github.com>
2022-08-31 21:06:16 +0100

    Add files via upload

---
+++ signup.tsx
@@ -0,0 +1,132 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useState } from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import useAuth from '../hooks/useAuth'
+
+interface Inputs {
+  email: string
+  password: string
+}
+
+function Login() {
+  const [login, setLogin] = useState(false)
+  const { signIn, signUp } = useAuth()
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>()
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    console.log(data)
+    if (login) {
+      await signIn(data.email, data.password)
+    } else {
+      await signUp(data.email, data.password)
+    }
+  }
+
+  
+
+  return (
+    <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
+      <Head>
+        <title>Netflix</title>
+        <link rel="icon" href="/pngwing.com.png" />
+      </Head>
+      <Image
+        src="/login.jpeg"
+        layout="fill"
+        className="-z-10 !hidden opacity-60 sm:!inline"
+        objectFit="cover"
+      />
+      <Link href={`/`}>
+        <img
+          src="/Netflix_2015_logo (1).svg"
+          className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
+          width={150}
+          height={150}
+        />
+      </Link>
+
+      <form
+        className="relative mt-20 space-y-12 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <h1 className="text-4xl font-semibold pt-5">Sign Up</h1>
+        <div className="space-y-4">
+          <label className="inline-block w-full">
+            <input
+              type="email"
+              placeholder="Email"
+              className={`input ${
+                errors.email && 'border-b-2 border-orange-500'
+              }`}
+              {...register('email', { required: true })}
+            />
+            {errors.email && (
+              <p className="p-1 text-[13px] font-light  text-orange-500">
+                Please enter a valid email.
+              </p>
+            )}
+          </label>
+          <label className="inline-block w-full">
+            <input
+              type="password"
+              maxLength={60}
+              {...register('password', { required: true })}
+              placeholder="Password"
+              className={`input ${
+                errors.password && 'border-b-2 border-orange-500'
+              }`}
+            />
+            {errors.password && (
+              <p className="p-1 text-[13px] font-light  text-orange-500">
+                Your password must contain between 4 and 60 characters.
+              </p>
+            )}
+          </label>
+        </div>
+        <button
+          className="w-full rounded bg-[#E50914] py-3 font-semibold"
+          onClick={() => setLogin(false)}
+          type="submit"
+        >
+          Sign Up
+        </button>
+        <div className="text-[gray]">
+          Have you an account?{' '}
+          <Link href={`/login`} >
+            <button
+              className="cursor-pointer text-white hover:underline"
+              type="submit"
+            >
+              Sign in 
+            </button>
+          </Link>
+          <br />
+          <p className="text-[13px] pt-3">
+            This page is protected by Google reCAPTCHA to ensure you're not a bot. 
+            <a className='text-[#196ED8] cursor-pointer '> Learn more.</a>
+          </p>
+          <p className="text-[13px] pt-3">
+            The information collected by Google reCAPTCHA is subject to the Google 
+            <a className='text-[#196ED8] cursor-pointer '> Privacy Policy </a>
+             and 
+            <a className='text-[#196ED8] cursor-pointer '> Terms of Service </a>
+            , and is used for providing, maintaining, and improving the reCAPTCHA service and for general security purposes (it is not used for personalized advertising by Google).
+          </p>
+
+
+        </div>
+      </form>
+          <hr className="text-[gray]"/>
+    </div>
+  )
+}
+
+export default Login
